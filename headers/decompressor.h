aed37d77aa3b279f1e4fe0ecf5da10365d9e2854
Sama-Ahmed-Mohammed <167349546+Sama-Ahmed-Mohammed@users.noreply.github.com>
2025-01-01 11:49:38 +0200

    Update decompressor.h

--- headers/decompressor.h
+++ headers/decompressor.h
@@ -10,20 +10,22 @@
 #include <string>
 
 // Define the Huffman Node structure
-struct Node {
+struct Node2 {
     char character;
     int frequency;
-    Node* left;
-    Node* right;
+    Node2* left;
+    Node2* right;
 
-    Node(char c, int freq);
-    Node(Node* left, Node* right);
+    Node2(char c, int freq) : character(c), frequency(freq), left(nullptr), right(nullptr) {}
+    Node2(Node2* left, Node2* right) : character(0), frequency(left->frequency + right->frequency), left(left), right(right) {}
 
-    bool operator>(const Node& other) const;
+    bool operator>(const Node2& other) const {
+        return frequency > other.frequency;
+    }
 };
 
 // Deserialize the Huffman Tree from a file
-Node* deserializeTree(std::ifstream& inFile);
+Node2* deserializeTree(std::ifstream& inFile);
 
 // Decompress the file and return the XML content
 void decompressXML(const std::string& compressedFilename, const std::string& outputFilename);
